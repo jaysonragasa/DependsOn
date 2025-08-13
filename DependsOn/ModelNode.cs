7191d7d5c71a75b40cfb16e287b52c97897b77bf
Jayson Ragasa <alfonsojayson.ragasa@infor.com>
2025-08-13 12:09:29 +0800

    Added dateTimeCreated so we can animate the node in visualizer

--- DependsOn/ModelNode.cs
+++ DependsOn/ModelNode.cs
@@ -7,14 +7,30 @@ public class Node
 	public string shortName { get; set; } = "";
 	public string project { get; set; } = "";
 	public string filePath { get; set; } = "";
+	public DateTime dateTimeCreated { get; set; } = DateTime.MinValue;
 
 	public static Node Create(int id, string name, string shortName, string project, string filePath)
-		=> new Node()
+	{
+		string file = string.Empty;
+		DateTime creationTime = DateTime.MinValue;
+
+		if (!string.IsNullOrWhiteSpace(filePath))
+		{
+			FileInfo fi = new FileInfo(filePath);
+			file = fi.FullName;
+			creationTime = fi.CreationTime;
+		}
+
+		var node = new Node()
 		{
 			id = id,
 			name = name,
 			shortName = shortName,
 			project = project,
-			filePath = filePath
+			filePath = file,
+			dateTimeCreated = creationTime,
 		};
+
+		return node;
+	}
 }
